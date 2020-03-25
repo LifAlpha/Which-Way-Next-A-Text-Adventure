9d172f1d346e3b60e06171b3a547cf49b0a4fa22
Caroline <62635799+LifAlpha@users.noreply.github.com>
2020-03-25 12:16:34 -0700

    Create which_way_next.py
    Basic intro & call to action added

---
+++ which_way_next.py
@@ -0,0 +1,32 @@
+#world rules
+yes_no = ["yes","no"]
+directions = ["left","right","forward","backward"]
+
+#introduction
+print = "The sound of an AC kicking on wakes you."
+time.sleep(2)
+print("A moment later, the soft dripping sounds of water from above pitter patter beside you."
+print("You find yourself on the ground in a dark damp alley."
+name = input("A voice whispers in your head. 'Who are you?'\n")
+print("'Hmm, " + name + ".'")
+time.sleep(3)
+print("When the voice speaks again, it feels as if it's right behind you. 'What a mundane title. Did you choose it yourself?', it hisses.")
+print("You turn around but nothing is there. It chuckles and the sound fades further into the darkness of the alleyway.")
+time.sleep(5)
+print("'GET UP. GET UP. GET UP. GET UP.' The voice screams into your ear, right beside you.)
+print("You jump to your feet. You are alone.")
+
+#call to action
+response = ""
+while response not in yes_no\n
+    response = input("The alley is damp but comfortable. It would be a shame to leave. What do you do?"
+    if response == "leave":
+        print("'Fool.' You step away from the cool brick and look out towards the street.")
+    elif response == "sit", "stay":
+        print("You sit back down beneath the dripping AC unit and listen to the steady drops of water fall beside you. After a while, you feel comfortable in that cold dark.")
+        quit()
+    else:
+        print("A soft chuckle whispers against your cheek. 'Do you stay or leave," + name + "?'")
+
+
+#part 1: a beginning
