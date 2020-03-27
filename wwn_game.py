2d9c2626a4ce6bbea7a2abb1ee87b4f0642e76e8
Caroline <62635799+LifAlpha@users.noreply.github.com>
2020-03-26 18:46:48 -0700

    Update wwn_game.py
    Added title screen

--- wwn_game.py
+++ wwn_game.py
@@ -1,1 +1,11 @@
+import os, time
+
+#title screen holds for 5 seconds
+print("Which Way Next: A Text Adventure Game\n")
+print("A Python Project by Caroline Bowman\n")
+print("Version 0.1\n")
+time.sleep(5)
+
+#clear the screen
+os.system('cls')
 
